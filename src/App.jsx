8e5295210bfe4302ed1dae7bd9fa65742cb2f08b
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-16 11:09:15 +0200

    Done some cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import socketio from 'socket.io-client'
-import Timer from './components/Timer';
-import './App.css';
 import { useEffect } from 'react';
-
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL) // denna ligger utanför App för att inte denna anslutningen ska köras två gånger. Hade den körts två gånger hade varje webbläsare fått två olika id´n eftersom den körst två gånger. 
+
+// styles
+import './App.css';
+
 
 const App = () => {
 
-/* 	// kör detta när appen mountas 
-	useEffect(() => {
-		socket.emit ('hello')
-	}, []) */
 
-/* 	const onClockStart = () => {
-	
-	}
-
-	const handleStartClock = () => {
-		socket.emit('clock:start')
-	} */
-	
 	return (
-		<div className="container text-center">
-			<Timer socket={socket} /> {/* skickar socket så den kan nås i Timer.jsx */}
+		<div>
+
 		</div>
 	)
 }
