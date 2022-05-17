253a7c37db6fffbca7590dfd7a1fe20deb34e768
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-17 15:27:18 +0200

    Renamed import styles to sass

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL) // denna ligger utanför App för att inte denna anslutningen ska köras två gånger. Hade den körts två gånger hade varje webbläsare fått två olika id´n eftersom den körst två gånger. 
 
 // styles
-import './App.css';
+import './App.scss';
 
 
 const App = () => {
