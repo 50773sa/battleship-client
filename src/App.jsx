852f843918a47c001e13673e52350a5a8abcfd8c
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-17 15:38:54 +0200

    Solved problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import socketio from 'socket.io-client'
-import { useEffect } from 'react';
-const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL) // denna ligger utanför App för att inte denna anslutningen ska köras två gånger. Hade den körts två gånger hade varje webbläsare fått två olika id´n eftersom den körst två gånger. 
-
 // styles
 import './App.scss';
+
+// import socketio from 'socket.io-client'
+import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react';
+
+// pages
+import StartGamePage from './pages/StartGamePage';
+import GameAreaPage from './pages/GameAreaPage';
+
+// const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL) 
 
 
 const App = () => {
 
-
 	return (
-		<div>
+	    <div id="App">
+		<div className='main-wrapper'>
+			<div className="box">
 
-		</div>
+			<Routes>
+				<Route path="/" element={<StartGamePage />} />
+				<Route path="/game" element={<GameAreaPage />} />
+							
+			</Routes>
+			</div>
+		</div>  
+    </div>
 	)
 }
 
