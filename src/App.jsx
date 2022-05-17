b51552cf888a6c269f1249062b41b56e22184266
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-17 13:49:49 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import socketio from 'socket.io-client'
+import Navigation from './components/Navigation'
+import { Routes, Route } from 'react-router-dom'
 import { useEffect } from 'react';
-import StartGamePage from './pages/StartGamePage';
-
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL) // denna ligger utanför App för att inte denna anslutningen ska köras två gånger. Hade den körts två gånger hade varje webbläsare fått två olika id´n eftersom den körst två gånger. 
 
+// pages
+import StartGamePage from './pages/StartGamePage';
+import GameAreaPage from './pages/GameAreaPage';
+
+
 // styles
-import './App.css';
+import './App.scss';
 
 
 const App = () => {
 
-
 	return (
-		<div>
-			{/* <StartGamePage /> */}
-			<GameAreaPage />
-		</div>
+	    <div id="App">
+		<Navigation />
+		<Container className='main-wrapper'>
+			<div className="box">
+
+			<Routes>
+				<Route path="/" element={<StartGamePage />} />
+				<Route path="/game" element={<GameAreaPage />} />
+							
+			</Routes>
+			</div>
+		</Container>  
+    </div>
 	)
 }
 
