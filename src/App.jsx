112a6a2584d8b511342dfd6ad2b4d0d6c2a7b31d
marialovgren <marialovgren86@hotmail.com>
2022-05-17 15:34:02 +0200

    created path for waitingroom

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 // styles
 import './App.scss';
 
-import socketio from 'socket.io-client'
-import Navigation from './components/Navigation'
 import { Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react';
+/* import { useEffect } from 'react'; */
 
 // pages
 import StartGamePage from './pages/StartGamePage';
 import GameAreaPage from './pages/GameAreaPage';
+import WaitingroomPage from './pages/WaitingroomPage';
 
-const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL) 
+/* const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL)  */
 
 
 const App = () => {
 
 	return (
 	    <div id="App">
-		<Navigation />
-		<Container className='main-wrapper'>
+		<div className='main-wrapper'>
 			<div className="box">
 
 			<Routes>
 				<Route path="/" element={<StartGamePage />} />
-				<Route path="/game" element={<GameAreaPage />} />
-							
+				<Route path="/waitingroom" element={<WaitingroomPage />} />
+				<Route path="/game" element={<GameAreaPage />} />	
 			</Routes>
 			</div>
-		</Container>  
+		</div>  
     </div>
 	)
 }
