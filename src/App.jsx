23350d8dddc97bf43d83cd2763d862712fb4cc70
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-17 11:42:43 +0200

    Renamed page and added <StartGamePage /> to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import socketio from 'socket.io-client'
 import { useEffect } from 'react';
+import StartGamePage from './pages/StartGamePage';
+
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL) // denna ligger utanför App för att inte denna anslutningen ska köras två gånger. Hade den körts två gånger hade varje webbläsare fått två olika id´n eftersom den körst två gånger. 
 
 // styles
@@ -11,7 +13,7 @@ const App = () => {
 
 	return (
 		<div>
-
+			<StartGamePage />
 		</div>
 	)
 }
