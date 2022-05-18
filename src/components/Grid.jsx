09fbc4dd4b37a527806c61bb22a7db0fc2ba802f
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-05-18 20:56:44 +0200

    Grid code added.

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
-export default function Grid() {
+export default function Grid(props) {
+
+
   return (
-    <div>
-      Här är Grid
-    </div>
+    <Container>
+        <Row className='row'>
+          {props.refs.map((text,index)=> (
+            <Col key={index}>
+              {text}
+            </Col>
+          ))}
+        </Row>
+        {props.rows.map((obj, index)=>{
+          <Row className='row' key={index}>
+            <Col key={index}>
+              {props.rows[index]}
+            </Col>
+            {props.columns.map((obj, index) =>(
+              <Col key={index}>
+                {props.rows[obj -1]}
+                {props.columns[index]}
+              </Col>
+          ))}
+          </Row>
+        })}
+    </Container>
   )
 }
+
+
--- src/pages/GameAreaPage.jsx
+++ src/pages/GameAreaPage.jsx
@@ -1,8 +1,47 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import Grid from '../components/Grid'
 
 
 function GameAreaPage() {
+	const [row, setRow] = useState([
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10"
+	])
+
+	const [col, setCol] = useState([
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+	])
+
+	const [ref, setRef] = useState([
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+	])
 
   return (
     <div>
@@ -22,7 +61,7 @@ function GameAreaPage() {
 					<p>Player 1</p>
 
 					<span>Ships left 4/4</span>
-					<div className="box"><Grid /></div> 
+					<div className="box"><Grid rows = {row} columns = {col} refs = {ref} /></div> 
 
 				</div>
 
@@ -47,8 +86,7 @@ function GameAreaPage() {
 					<p>Player 2</p>
 
 					<span>Ships left 4/4</span>
-					<div className="box"><Grid /></div>
-
+					<div className="box"><Grid rows = {row} columns = {col} refs = {ref} /></div> 
 
 				</div>
 			</section>
