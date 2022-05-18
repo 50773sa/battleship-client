92a953e3050ef5bed3f3beb82ae63bb68d7dc01a
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-05-18 14:00:39 +0200

    merged main into my branch

--- src/pages/StartGamePage.jsx
+++ src/pages/StartGamePage.jsx
@@ -1,14 +1,40 @@
-import { Link } from 'react-router-dom'
+import { useEffect, /* useState */ } from 'react'
+import { Link, useNavigate } from 'react-router-dom' 
 
-function StartGamePage() {
+function StartGamePage( { socket } ) { // tar emot socket från App.jsx
+ 	/* const [player1, setPlayer1] = useState(null)
+	const [player2, setPlayer2] = useState(null)  */
 
+
+	const navigate = useNavigate()
+
+
+	const handlePlayerClickedBtn = () => {
+		console.log(`Client ${socket.id} wants to join the game`)
+		socket.emit('join:game') // eventet emitas till servern som sedan ska skicka tillbaka
+	}
+
+	// funktion för att kolla om  det finns en eller två spelare som vill spela
+	const onJoinGame = () => {
+		navigate('/game')
+	}
+
+	useEffect(() => {
+		// lyssna efter join:game events från servern
+		socket.on('join:game', onJoinGame)
+	}, [socket]) // skicka in navigate också som dependecy????
 
 
   return (
     <div className='joinGameWrapper'>
         <div className="joinGameBox">
             <p>Battleship</p>
-            <button className='btn startGameBtn' as={Link} to={'/waitingroom'}>Join Game</button>
+				<button
+				 	onClick={handlePlayerClickedBtn}  
+					as={Link}
+					className='btn startGameBtn'
+					> Join Game
+				</button>
         </div>
     </div>
   )
