4a662891040464e69b60ed6549bdc8c702027f25
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-05-25 14:18:39 +0200

    test

--- src/hooks/useGetShips.js
+++ src/hooks/useGetShips.js
@@ -25,28 +25,52 @@ function useGetShips() {
         position: [], 
       }
     ]
+    // Random function
+    const getRandomIndex = (array, blocks) => {
+      const randomId = Math.floor(Math.random() * (array.length - blocks));
+      return array[randomId];
+    }
 
     const setPositions = () => {
-      ships.map((ship) => {
-        const randomIndexCol = Math.floor(Math.random() * (columns.length - ship.block))
-        const randomIndexRow = Math.floor(Math.random() * rows.length)
-        
-        let col = columns[randomIndexCol]
-        const row = rows[randomIndexRow]
+      ships.forEach((ship) => {
+      
+        let col = getRandomIndex(columns, ship.block)
+        let row = getRandomIndex(rows, 0)
 
-        // Repeat for the number of blocks.
-        for (let i = 0; i < ship.block; i++){ 
-          ship.position.push(col.toString() + row)
+        console.log('ships position', ship.position)
+
+        let hasDuplicates = ships.some(({ position }) =>
+          position.some(
+            (positions) => 
+              positions === col + row ||
+              positions === col + 1 + row ||
+              positions === col - 1 +row
+          ) 
+        )
+
+        if (hasDuplicates) {
+          col = getRandomIndex(columns, ship.block)
+          console.log('collision!')
+          console.log('new col:', col)
+        }
+        while (ship.position.length < ship.block){
+          ship.position.push(col + row)
           col = col +1
         }
+
         
-        return ships
+
+        // Repeat for the number of blocks.
+        // for (let i = 0; i < ship.block; i++){ 
+        //   ship.position.push(col.toString() + row)
+        //   col = col +1
+        // }
+        
       })
+      return { ships }
     } 
 
     setPositions()
-
-  return{ ships }
 }
 
 export default useGetShips
