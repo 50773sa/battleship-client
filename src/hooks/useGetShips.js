16d2a7c5c0b17d8f4ff1c8fb511e867c161656f2
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-26 19:27:40 +0200

    Moved ship array to GameContextProvider

--- src/hooks/useGetShips.js
+++ src/hooks/useGetShips.js
@@ -1,52 +1,67 @@
 import { useEffect, useState } from 'react'
 import { columns, rows }  from './useCellIds';
+import { useGameContext } from '../contexts/GameContextProvider'
+
 
 function useGetShips() {
+  const { ships } = useGameContext()
 
-    const ships = [
-      {
-        name: "shipA",
-        block: 4,
-        position: [],        
-      },
-      {
-        name: "shipB",
-        block: 3,
-        position: [], 
-      },
-      {
-        name: "shipC",
-        block: 2,
-        position: [], 
-      },
-      {
-        name: "shipD",
-        block: 2,
-        position: [], 
+  console.log('SHIPS',ships)
+
+      // Random function
+      const getRandomIndex = (array, blocks) => {
+        const randomId = Math.floor(Math.random() * (array.length - blocks));
+        return array[randomId];
       }
-    ]
-
-    const setPositions = () => {
-      ships.map((ship) => {
-        const randomIndexCol = Math.floor(Math.random() * (columns.length - ship.block))
-        const randomIndexRow = Math.floor(Math.random() * rows.length)
+  
+      const setPosition = () => {
+        ships.forEach((ship) => {
         
-        let col = columns[randomIndexCol]
-        const row = rows[randomIndexRow]
+          let col = getRandomIndex(columns, ship.block)
+          let row = getRandomIndex(rows, 0)
+  
+          console.log('ships position', ship)
+  
+          let hasDuplicates = ships.some(({ position }) =>
+            position.some(
+              (positions) => 
+                positions === col + row ||
+                positions === col + 1 + row ||
+                positions === col - 1 +row
+            ) 
+          )
+  
+          if (hasDuplicates) {
+            col = getRandomIndex(columns, ship.block)
+            console.log('collision!')
+            console.log('new col:', col)
+          }
+          while (ship.position.length < ship.block){
+            ship.position.push(col + row)
+            col = col +1
+          }
+  
+          
+  
+          // Repeat for the number of blocks.
+          // for (let i = 0; i < ship.block; i++){ 
+          //   ship.position.push(col.toString() + row)
+          //   col = col +1
+          // }
+          
+        })
 
-        // Repeat for the number of blocks.
-        for (let i = 0; i < ship.block; i++){ 
-          ship.position.push(col.toString() + row)
-          col = col +1
-        }
-        
-        return ships
-      })
-    } 
-
-    setPositions()
-
-  return{ ships }
+        return (
+          <div className="ships">
+            { ships }
+          </div>
+        )
+      } 
+      useEffect(() => {
+        setPosition(ships)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      },[ships])
+  
 }
-
-export default useGetShips
+  
+  export default useGetShips
