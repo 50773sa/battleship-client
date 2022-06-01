788b612473263b465888a17ee2377f1ec57b2078
50773sa <93046590+50773sa@users.noreply.github.com>
2022-06-01 14:23:20 +0200

    Added 'shipPosition' and place in 'ships-state'

--- src/contexts/GameContextProvider.jsx
+++ src/contexts/GameContextProvider.jsx
@@ -1,5 +1,7 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import socketio from "socket.io-client";
+import useGetShips from '../hooks/useGetShips';
+import useCellIds from '../hooks/useCellIds';
 
 const GameContext = createContext()
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL);
@@ -15,6 +17,8 @@ const GameContextProvider = ({ children }) => {
 	const [opponent, setOpponent] = useState("") 
     const [myTurn, setMyTurn] = useState()
     const [ships, setShips] = useState ([])
+    const [shipPosition, setShipPosition] = useState(useGetShips())
+	const ids = useCellIds()
 
 
     const values = {
@@ -29,9 +33,17 @@ const GameContextProvider = ({ children }) => {
         setOpponent,
         myTurn,
         setMyTurn,
-        ships,
-        setShips
+        ships,    
+        ids,
     }
+
+
+	//** Place the ships when page is mounted **/
+	useEffect(() => {		
+		setShipPosition(shipPosition)
+		setShips(shipPosition)
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	},[])
 
     return (
         <GameContext.Provider value={values}>
