69c5392bf86b6fe25b7128e519df8f33eca1c2d6
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-06-08 14:32:52 +0200

    fetched Saras code into battleboard

--- src/components/Battleboard.jsx
+++ src/components/Battleboard.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react'
 import { useGameContext } from '../contexts/GameContextProvider'
+import { useEffect } from 'react'
 
 
-export default function Battleboard({ hasShip }) {
-	const { ships, ids } = useGameContext()
+export default function Battleboard() {
+	const { ships, ids, socket} = useGameContext()
 
 	const BattleboardCell = ({ hasShip }) =>{
 		
@@ -12,15 +12,41 @@ export default function Battleboard({ hasShip }) {
 		  )
 	}
 
-	
-
-
 	useEffect(() => {
-	/* 	const data = {
-			player,
-			ships
-		} */
+		const shipA = ships[0]
+
+		// STEG 4. Ta emot cell id från battleboard via servern. 
+		socket.on('receive:shot', function (cellId, otherPlayer) {
+
+			console.log("Ship A POSITION: ", shipA.position)
+
+			if(shipA.position.includes(cellId)) {
+				console.log("You clicked on SHIP A", shipA.position.includes(cellId))
+				console.log("Opponent that hit me was ", otherPlayer)
+
+				// emit shot:result, hit = true
+			} else {
+				console.log("You missed!", cellId)
+
+				// emit shot:result, hit = false
+			} 
+		})
+	}, [ships, socket])
+
+	/* const handleReceiveShot = useCallback((data) => {
+		const shipA = ships[0]
+		console.log("STEP 4: Receiving cell id in Battleboard: ", data)
+		console.log("Ship A: ", shipA.position.includes(data))
+		console.log("Ship A POSITION: ", shipA.position)
+		if(shipA.position.includes(data)) {
+			console.log("You clicked on SHIP A", shipA.position.includes(data))
+		} else {
+			console.log("You missed!", data)
+		} 
 	}, [])
+	// STEG 4. Ta emot cell id från battleboard via servern. 
+	socket.on('receive:shot', handleReceiveShot) */
+	
 
  	return (
 		<div className='cell'>
--- src/components/OpponentBattleboard.jsx
+++ src/components/OpponentBattleboard.jsx
@@ -1,19 +1,37 @@
 import { useGameContext } from '../contexts/GameContextProvider' 
 
-export default function OpponentBattleboard({ id, hasShip }) {
 
-	const { ids } = useGameContext()
+export default function OpponentBattleboard() {
+	const { ids, socket, otherPlayer } = useGameContext()
 
 
 	const OpponentBattleboardCell = ({ id }) => {		
-			return(
-				<div className='defaultCellColor' 
-					onClick={() => {
-						console.log(`Clicked ${id}`)
-				}} 
-				/>	
-			)
+		return(
+			<div className='defaultCellColor' 
+				onClick={() => {
+					console.log(`Clicked ${id}`)
+			}} 
+			/>	
+		)
+	}	
+
+	const handleShotFired = (e) => {
+		e.preventDefault()
+
+		const cellId = e.target.id
+		console.log(`STEP 1: I clicked on ${cellId}. Opponent is `, otherPlayer)
+
+		// STEG 1. Skicka id på den ruta som spelaren klickat på till servern. 
+		socket.emit('player:shot', cellId, otherPlayer)		
 	}
+
+	// when mounted, listen for final:result event and update this battleboard with hit/miss
+
+	/* const data = {
+		player,
+		ships
+	}  */
+
 
 	return (
 		<div className='cell'>
