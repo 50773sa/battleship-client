62c7e7a80a2472ab2cbbf5eb8f0f2e9d8591a50a
50773sa <93046590+50773sa@users.noreply.github.com>
2022-06-08 10:27:45 +0200

    Added useEffect()

--- src/components/Battleboard.jsx
+++ src/components/Battleboard.jsx
@@ -1,25 +1,41 @@
 import { useGameContext } from '../contexts/GameContextProvider'
+import { useEffect } from 'react'
 
 
 export default function Battleboard() {
 	const { ships, ids, socket} = useGameContext()
 
-	const shipA = ships[0]
+	useEffect(() => {
+		const shipA = ships[0]
 
-	const handleReceiveShot = (data) => {
+		// STEG 4. Ta emot cell id från battleboard via servern. 
+		socket.on('receive:shot', function (data) {
+			
+			console.log("Ship A POSITION: ", shipA.position)
+
+			if(shipA.position.includes(data)) {
+				console.log("You clicked on SHIP A", shipA.position.includes(data))
+			} else {
+				console.log("You missed!", data)
+			} 
+		})
+	}, [ships, socket])
+
+	/* const handleReceiveShot = useCallback((data) => {
+		const shipA = ships[0]
 		console.log("STEP 4: Receiving cell id in Battleboard: ", data)
 		console.log("Ship A: ", shipA.position.includes(data))
 		console.log("Ship A POSITION: ", shipA.position)
 
-		if(data === shipA.position.includes(data)) {
+		if(shipA.position.includes(data)) {
 			console.log("You clicked on SHIP A", shipA.position.includes(data))
 		} else {
 			console.log("You missed!", data)
 		} 
-	}
+	}, [])
 
 	// STEG 4. Ta emot cell id från battleboard via servern. 
-	socket.on('receive:shot', handleReceiveShot)
+	socket.on('receive:shot', handleReceiveShot) */
 	
  	return (
 		<div className='cell'>
